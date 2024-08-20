8a046298cd57e805c940ae787f30c8c8b0b685dc
Lucas-O-S <lucasoscomercial@gmail.com>
2024-08-20 12:25:07 -0300

    Adição de eventos JS para o banner mudar e botão de para e contnuar evento

--- Mirror Fashion/JS/home.js
+++ Mirror Fashion/JS/home.js
@@ -1,12 +1,42 @@
 
-function validaBusca(){
 
-    if(document.querySelector('#q').value == ''){
+//Verifica se a texto antes de fazer a busca
+document.querySelector('#form-busca').onsubmit = function(){
+    if(document.querySelector("#q").value == ""){
         alert('Não podia ter deixado em branco a busca!');
         document.querySelector('#form-busca').style.background = 'red';
         return false;
     }
+ 
+};
+
+//array de imagens de destaque
+var banners = ["../img/destaque-home.png", "../img/destaque-home-2.png"];
+
+//Banner que está sendo mostrado
+bannerAtual = 0;
+
+//Função que troca a imagem do banner
+function TrocarBanner(){
+    bannerAtual = (bannerAtual+1)%2;
+    document.querySelector('.destaque img').src = banners[bannerAtual];
+
 }
 
+//Variavel de timer que ativa a funçaõ de trocar o banner
+var timer = setInterval(TrocarBanner,4000); 
 
-document.querySelector('#form-busca').onsubmit = validaBusca;
+var controle = document.querySelector(".pause");
+
+controle.onclick = function(){
+    if(controle.className == 'pause'){
+        clearInterval(timer);
+        controle.className = 'play';
+    }
+    else{
+        timer = setInterval(TrocarBanner,4000);
+        controle.className = 'pause';
+    }
+    return false;
+}
+
