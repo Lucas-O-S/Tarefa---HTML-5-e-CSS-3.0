8b5fc64d4a70d06d0318c2ba8d0223e9e5b4d7ae
Lucas-O-S <lucasoscomercial@gmail.com>
2024-08-20 14:10:14 -0300

    Melhorias do css

--- Mirror Fashion/JS/home.js
+++ Mirror Fashion/JS/home.js
@@ -26,8 +26,11 @@ function TrocarBanner(){
 //Variavel de timer que ativa a funçaõ de trocar o banner
 var timer = setInterval(TrocarBanner,4000); 
 
+//Variavel de controle de pause
 var controle = document.querySelector(".pause");
 
+
+//Controla a função de trocar banner atravez de um botão
 controle.onclick = function(){
     if(controle.className == 'pause'){
         clearInterval(timer);
